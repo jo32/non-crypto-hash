7edb8b4f041f1ff978315f2af4be744242c886da
JIANG DL <j.jolam@gmail.com>
2015-10-28 15:13:51 +0800

    fix dynamic require problem in webpack

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var path = require('path');
 
+var algorithms = {
+    superfasthash: require('./libs/superfasthash'),
+    murmurhash3: require('./libs/murmurhash3')
+}
+
 module.exports = {
     createHash: function createHash(algorithm) {
-        algorithm = algorithm.replace(/![a-zA-z0-9]/g, "");
+        algorithm = algorithm.replace(/![a-zA-z0-9]/g, "").toLowerCase();
         try {
-            return require(path.join(__dirname, 'libs', algorithm));
+            return algorithms[algorithm];
         } catch (e) {
             throw new Error(e);
         }
