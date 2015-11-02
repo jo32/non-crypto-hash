f3c49dbdd1adf549cc451ebcce4f956d401f2aa8
JIANG DL <j.jolam@gmail.com>
2015-11-02 12:26:39 +0800

    fix bugs of string to u8int array

--- libs/util.js
+++ libs/util.js
@@ -1,14 +1,17 @@
 function toU8IntArray(inputString) {
     var array = [];
+    var tempArrayOfChar = [];
     for (var i = 0; i < inputString.length; i++) {
         var charCode = inputString.charCodeAt(i);
         var temp;
+        tempArrayOfChar.length  = 0;
         while ((temp = charCode & 0xff) || charCode) {
             // pushing one byte
-            array.push(temp);
+            tempArrayOfChar.push(temp);
             // shifting 8 bit right
             charCode >>>= 8;
         }
+        array = array.concat(tempArrayOfChar.reverse());
     }
     return array;
 }
@@ -16,11 +19,13 @@ function toU8IntArray(inputString) {
 function intToUnsignedHex(number) {
     var temp;
     var hexArray = [];
-    while ((temp = number & 0xff) || number) {
-        hexArray.push(temp.toString(16));
-        number >>>= 8;
+    while ((temp = number & 0xffff) || number) {
+        temp = temp.toString(16);
+        temp = '0000'.slice(temp.length) + temp;
+        hexArray.push(temp);
+        number >>>= 16;
     }
-    return hexArray.join('');
+    return hexArray.reverse().join('');
 }
 
 module.exports = {
